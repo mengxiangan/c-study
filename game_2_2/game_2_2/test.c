7ef2856b80b2905f8c3b1ccab02e0b30092bb5d9
云费大大 <1952657776@qq.com>
2023-02-02 21:41:19 +0800

    更新代码

---
+++ game_2_2/game_2_2/test.c
@@ -0,0 +1,30 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include"game.h"
+
+void menu() {
+	printf("***************************\n");
+	printf("***   1:扫雷游戏     ******\n");
+	printf("***   0:退出游戏     ******\n");
+	printf("***************************\n");
+}
+
+int main() {
+	int input = 0;
+	do {
+		menu();
+		printf("***：>");
+		scanf("%d", &input);
+		switch (input) {
+		case 1:
+			printf("扫雷\n");
+			break;
+		case 0:
+			printf("退出游戏\n");
+			break;
+		default:
+			printf("输入错误，请重新输入！\n");
+			break;
+		}
+	} while(input);
+	return 0;
+}
